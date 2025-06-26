08dd2f3a0afb1d1a96f69a91d53fcd84d2e92e87
Dheerendra Verma <201247410+dheeru11291@users.noreply.github.com>
2025-06-26 20:46:23 +0530

    Add files via upload

---
+++ problem1/1.js
@@ -0,0 +1,7 @@
+const productPen ={
+    name: "Reynolds Metal Pens (Iconic Elite Gun Metal) | Premium Pens For Gifting | Twist Pens For Personal & Professional Use | Stylish Gifts for Men & Women | Trim Ball Pens For Corporate Gifting | Blue Ink",    
+    rating: 4.3,
+    price: "$1.6",
+    off: '40%'
+}
+console.log(productPen);
---
+++ problem2/2.js
@@ -0,0 +1,9 @@
+const profile={
+    userName: "dheeru__4",
+    name: "Dheerendra Verma",
+    followers: 120,
+    following: 122,
+    post:1,
+
+};
+console.log(profile)
